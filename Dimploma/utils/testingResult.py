b925001398b765213d480e0a3c7ccff9065ff80d
Milos <>
2025-03-24 12:54:46 +0100

    Optimize testing

--- Dimploma/utils/testingResult.py
+++ Dimploma/utils/testingResult.py
@@ -11,26 +11,34 @@ class TestResult:
         self.agent_names = []
         self.agent_colors = []
         self.agents = []
+        self.agent_multiple_tests = []
         self.objs = np.zeros(test_amount, dtype=np.float32)
         self.rews = np.zeros(test_amount, dtype=np.float32)
         self.actions = np.zeros((test_amount, node_amount * 2), dtype=np.int16)
 
-    def addAgent(self, name, agent: MyAgent, color='blue'):
+    def addAgent(self, name, agent: MyAgent, color='blue', multiple_tetst=False):
         self.agent_names.append(name)
         self.agents.append(agent)
         self.agent_colors.append(color)
+        self.agent_multiple_tests.append(multiple_tetst)
 
     def test(self, env):
         ags = len(self.agents)
         self.objs = np.zeros((ags, self.test_amount), dtype=np.float32)
         self.rews = np.zeros((ags, self.test_amount), dtype=np.float32)
         self.actions = np.zeros((ags, self.test_amount, self.node_amount * 2), dtype=np.int16)
-        for i in range(self.test_amount):
-            for j, agent in enumerate(self.agents):
+        for j, agent in enumerate(self.agents):
+            if self.agent_multiple_tests[j]:
+                for i in range(self.test_amount):
+                    obj, sel, rew, acts = agent.test(env)
+                    self.objs[j, i] = obj
+                    self.rews[j, i] = rew[-1]
+                    self.actions[j, i, :len(acts)] = acts
+            else:
                 obj, sel, rew, acts = agent.test(env)
-                self.objs[j, i] = obj
-                self.rews[j, i] = rew[-1]
-                self.actions[j, i, :len(acts)] = acts
+                self.objs[j, :] = obj
+                self.rews[j, :] = rew[-1]
+                self.actions[j, :, :len(acts)] = acts
 
 
     def print_result(self, rews=False):
